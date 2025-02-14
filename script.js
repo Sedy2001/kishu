1887e846c81a160768f946d6c8db8274a77859ac
Sedy2001 <153453736+Sedy2001@users.noreply.github.com>
2025-02-13 22:40:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ function startGame() {
     imgTarget.onload = function() {
         targetWidth = imgTarget.width * scaleFactor;
         targetHeight = imgTarget.height * scaleFactor;
-        moveInterval = setInterval(moveTargetRandomly, 750); // Move every 500 milliseconds for faster movement
+        moveInterval = setInterval(moveTargetRandomly, 1000); // Move every 1000 milliseconds for faster movement
     };
 
     backgroundImage.onload = function() {
